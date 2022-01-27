842d38341fcd0296e315b0af04a7f8e4fb8acb5b
Aleksi Bovellan <aleksi21009@student.hamk.fi>
2022-01-27 04:22:43 +0200

    btn_clicks to change box_results

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,51 +27,60 @@ namespace WPF_laskin
 
         private void btn_1_Click(object sender, RoutedEventArgs e)
         {
-
+            box_results.Text = "1";
         }
 
         private void btn_2_Click(object sender, RoutedEventArgs e)
         {
+            box_results.Text = "2";
 
         }
 
         private void btn_3_Click(object sender, RoutedEventArgs e)
         {
+            box_results.Text = "3";
 
         }
 
         private void btn_4_Click(object sender, RoutedEventArgs e)
         {
+            box_results.Text = "4";
 
         }
 
         private void btn_5_Click(object sender, RoutedEventArgs e)
         {
+            box_results.Text = "5";
 
         }
 
         private void btn_6_Click(object sender, RoutedEventArgs e)
         {
+            box_results.Text = "6";
 
         }
 
         private void btn_7_Click(object sender, RoutedEventArgs e)
         {
+            box_results.Text = "7";
 
         }
 
         private void btn_8_Click(object sender, RoutedEventArgs e)
         {
+            box_results.Text = "8";
 
         }
 
         private void btn_9_Click(object sender, RoutedEventArgs e)
         {
+            box_results.Text = "9";
 
         }
 
         private void btn_equal_Click(object sender, RoutedEventArgs e)
         {
+            box_results.Text = "OK";
 
         }
     }
